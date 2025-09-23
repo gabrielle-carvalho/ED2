4c68d07037eafb8f6189f8d213604632f3feeafa
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2025-09-23 08:59:21 -0300

    Create hasing_linear.py

---
+++ hashing/hasing_linear.py
@@ -0,0 +1,14 @@
+def __init__():
+    self.tam=8
+    self.lista=[tam]
+
+def hash():
+    s = self.num%2
+    return s
+
+def insert_hashing(num):
+    self.pos = hash(num)
+    if lista[0]:
+        lista[0]=num
+    else:
+        for i in range(tam):
